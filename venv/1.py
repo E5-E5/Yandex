b09d89632e7d1809123e213ca817eb2181938bcb
USER <sasha533.laz2015@mail.ru>
2021-01-08 18:46:13 +0300

    Оно работает, я в шоке

--- venv/1.py
+++ venv/1.py
@@ -1,6 +1,8 @@
 import os
 import pygame
 
+next = False
+s = 1
 
 def load_image(name, color_key=None):
     fullname = os.path.join('data', name)
@@ -20,11 +22,24 @@ def load_image(name, color_key=None):
 
 
 class Mountain(pygame.sprite.Sprite):
-    image = load_image("C:/Users/USER/Desktop/piton/RABOTI/12.png", -1)
+    image = load_image(f"C:/Users/USER/Desktop/piton/RABOTI/Игра/start{s}.png", -1)
 
     def __init__(self):
         super().__init__(all_sprites)
         self.image = Mountain.image
+        self.rect = self.image.get_rect()
+        # вычисляем маску для эффективного сравнения
+        self.mask = pygame.mask.from_surface(self.image)
+        # располагаем горы внизу
+        self.rect.bottom = height
+
+
+class Finish(pygame.sprite.Sprite):
+    image = load_image(f"C:/Users/USER/Desktop/piton/RABOTI/Игра/finish{s}.png", -1)
+
+    def __init__(self):
+        super().__init__(all_sprites)
+        self.image = Finish.image
         self.rect = self.image.get_rect()
         # вычисляем маску для эффективного сравнения
         self.mask = pygame.mask.from_surface(self.image)
@@ -47,24 +62,33 @@ class Landing(pygame.sprite.Sprite):
     def update(self):
         if pygame.sprite.collide_mask(self, mountain):
             pygame.mouse.set_pos(450, 500)
+        if pygame.sprite.collide_mask(self, finish):
+            global next
+            next = True
 
 
 pygame.init()
 
-size = width, height = 600, 600
+size = width, height = 750, 750
 screen = pygame.display.set_mode(size)
 
 # группа, содержащая все спрайты
 all_sprites = pygame.sprite.Group()
 
 mountain = Mountain()
-
+finish = Finish()
 clock = pygame.time.Clock()
 land = Landing(500, 500)
 running = True
 pygame.mouse.set_visible(False)
 pygame.mouse.set_pos(450, 500)
 while running:
+    # в теории эдо должен быть переход на след уровень, но почему так не работает
+    if next:
+        s += 1
+        finish = Finish()
+        mountain = Mountain()
+        next = False
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
